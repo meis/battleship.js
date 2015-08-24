2cdbdab84c3cd61969652943f15e9c370b6f1b76
Marc Egea i Sala <marc.egea.sala@gmail.com>
2015-08-24 20:59:53 +0200

    Add method to get all squares neighbours

--- src/model/Board.js
+++ src/model/Board.js
@@ -21,6 +21,30 @@ export default class Board {
     return this.squares[x][y];
   }
 
+  neighbourSquares(name) {
+    let [x, y] = this.squareCoordsFromName(name);
+
+    let neighbours = new Set();
+    let movements  = [
+      [-1,-1], [-1, 0], [-1, 1],
+      [ 0,-1]         , [ 0, 1],
+      [ 1,-1], [ 1, 0], [ 1, 1],
+    ];
+
+    // Try all possible movements and add them to
+    // the Set if are within the valid values
+    movements.forEach( m => {
+      let mX = x + m[0];
+      let mY = y + m[1];
+
+      if (mX >= 0 && mX < this.N && mY >= 0 && mY < this.N) {
+        neighbours.add(this.squareNameFromCoords(mX, mY));
+      }
+    } );
+
+    return neighbours;
+  }
+
   // Convert [0, 0] to 'A1'
   squareNameFromCoords(x, y) {
     return String.fromCharCode(charStart + x) + `${y + 1}`;
--- test/model/Board.js
+++ test/model/Board.js
@@ -31,4 +31,28 @@ describe('Board', () => {
     assert.equal(b.getSquare('A1').name, 'A1');
     assert.equal(b.getSquare('J10').name, 'J10');
   });
+
+  it('computes the neighbours of a square', () => {
+    let b = new Board();
+
+    let neighboursA1 = b.neighbourSquares('A1');
+
+    assert.equal(neighboursA1.size, 3);
+    assert.ok(neighboursA1.has('A2'));
+    assert.ok(neighboursA1.has('B2'));
+    assert.ok(neighboursA1.has('B1'));
+    assert.notOk(neighboursA1.has('A3'));
+
+    let neighboursD4 = b.neighbourSquares('D4');
+
+    assert.equal(neighboursD4.size, 8);
+    assert.ok(neighboursD4.has('C3'));
+    assert.ok(neighboursD4.has('C4'));
+    assert.ok(neighboursD4.has('C5'));
+    assert.ok(neighboursD4.has('D3'));
+    assert.ok(neighboursD4.has('D5'));
+    assert.ok(neighboursD4.has('E3'));
+    assert.ok(neighboursD4.has('E4'));
+    assert.ok(neighboursD4.has('E5'));
+  });
 });
