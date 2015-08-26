30a3a40aac6e37b4ed3c8b370aceac4fdb218f37
Marc Egea i Sala <marc.egea.sala@gmail.com>
2015-08-26 10:23:09 +0200

    Script to play Battleship :)

--- dist/index.js
+++ dist/index.js
@@ -12,29 +12,42 @@ var _prompt2 = _interopRequireDefault(_prompt);
 
 var b = new _modelBoard2['default'](5, 4, 4);
 
+_prompt2['default'].message = "";
+_prompt2['default'].delimiter = "";
+_prompt2['default'].colors = false;
 _prompt2['default'].start();
 
-var ask = function ask() {
-  _prompt2['default'].get(['command'], function (err, result) {
-    if (result.command != 'EXIT') {
-      var shot = b.shot(result.command);
+var property = {
+  name: 'command',
+  message: "Enter a square to shot or 'EXIT' to finish:"
+};
 
-      if (shot) {
-        if (shot.sunk) {
-          console.log('Sunk!');
-        } else if (shot.hit) {
-          console.log('Hit!');
-        } else {
-          console.log('Miss!');
-        }
-        if (!b.finished()) {
-          ask();
+var ask = function ask(board) {
+  _prompt2['default'].get([property], function (err, result) {
+    if (result.command != 'EXIT' && result.command != 'exit') {
+      if (board.validShot(result.command)) {
+        var shot = board.shot(result.command);
+
+        if (shot) {
+          if (shot.sunk) {
+            console.log('Hit!! The boat is sunk!'.green.bold);
+          } else if (shot.hit) {
+            console.log('You hit a boat!'.green);
+          } else {
+            console.log('Miss!'.blue);
+          }
         }
       } else {
-        console.log('Enter a valid location');
+        console.log(('Invalid square ' + result.command + '. Please entry a valid one (ex: A5)').red);
+      }
+
+      if (board.finished()) {
+        console.log('You win! All ships are sunk!'.green.bold);
+      } else {
+        ask(board);
       }
     }
   });
 };
 
-ask();
+ask(b);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
-import Board from './model/Board';
+import Board  from './model/Board';
+import prompt from 'prompt';
 
-let b = new Board();
-console.log(b.constructor.name);
+let b = new Board(5, 4, 4);
+
+prompt.message   = "";
+prompt.delimiter = "";
+prompt.colors    = false;
+prompt.start();
+
+let property = {
+  name   : 'command',
+  message: "Enter a square to shot or 'EXIT' to finish:",
+}
+
+let ask = function(board) {
+  prompt.get([property], function(err, result) {
+    if (result.command != 'EXIT' && result.command != 'exit') {
+      if (board.validShot(result.command)) {
+        let shot = board.shot(result.command);
+
+        if (shot) {
+          if (shot.sunk) {
+            console.log('Hit!! The boat is sunk!'.green.bold);
+          }
+          else if (shot.hit) {
+            console.log('You hit a boat!'.green);
+          }
+          else {
+            console.log('Miss!'.blue);
+          }
+        }
+      }
+      else {
+        console.log(`Invalid square ${result.command}. Please entry a valid one (ex: A5)`.red);
+      }
+
+      if (board.finished()) {
+        console.log('You win! All ships are sunk!'.green.bold);
+      }
+      else {
+        ask(board);
+      }
+    }
+  });
+}
+
+ask(b);
